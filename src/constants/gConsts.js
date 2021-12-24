5442afc1c09bf77416e1eafce52be17cef0d71ab
harshmevada <harshmewada056@gmail.com>
2021-12-24 17:17:07 +0530

    added .env

--- src/constants/gConsts.js
+++ src/constants/gConsts.js
@@ -2,18 +2,20 @@
 
 //Application related constants
 export const REACT_APP_GOOGLE_OAUTH_SCOPES =
-  "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/classroom.courses.readonly https://www.googleapis.com/auth/classroom.coursework.me https://www.googleapis.com/auth/classroom.rosters.readonly https://www.googleapis.com/auth/drive";  
+  process.env.REACT_APP_GOOGLE_OAUTH_SCOPES;
 
-export const REACT_APP_GOOGLE_OAUTH_TUTOR_SCOPES = "https://www.googleapis.com/auth/classroom.coursework.students https://www.googleapis.com/auth/classroom.courses https://www.googleapis.com/auth/classroom.coursework.students https://www.googleapis.com/auth/classroom.rosters";
+export const REACT_APP_GOOGLE_OAUTH_TUTOR_SCOPES =
+  "https://www.googleapis.com/auth/classroom.coursework.students https://www.googleapis.com/auth/classroom.courses https://www.googleapis.com/auth/classroom.coursework.students https://www.googleapis.com/auth/classroom.rosters";
 
-export const GOOGLE_USERINFO_API  = "https://www.googleapis.com/userinfo/v2/";
+export const GOOGLE_USERINFO_API = "https://www.googleapis.com/userinfo/v2/";
 
 export const GOOGLE_CLASSROOM_API = "https://classroom.googleapis.com/v1/";
 
-export const GOOGLE_DRIVE_API        = "https://www.googleapis.com/drive/v3/"
-export const GOOGLE_DRIVE_UPLOAD_API = "https://www.googleapis.com/upload/drive/v3/"
+export const GOOGLE_DRIVE_API = "https://www.googleapis.com/drive/v3/";
+export const GOOGLE_DRIVE_UPLOAD_API =
+  "https://www.googleapis.com/upload/drive/v3/";
 
-export const GOOGLE_PEOPLE_API = "https://people.googleapis.com/v1/"
+export const GOOGLE_PEOPLE_API = "https://people.googleapis.com/v1/";
 
 //features/ functionality related constants
 export const COURSE_ACTIVE = "ACTIVE";
@@ -21,46 +23,44 @@ export const COURSE_ARCHIVED = "ARCHIVED";
 
 //Drive file types
 export const driveFileTypes = {
-  DRIVE_FORMS : 'Forms',
-  DRIVE_DOCS : 'Docs',
-  DRIVE_SLIDES : 'Presentation',
-  DRIVE_SHEETS : 'Spreadsheets',
-  DRIVE_DRAWINGS : 'Drawings',
-}
+  DRIVE_FORMS: "Forms",
+  DRIVE_DOCS: "Docs",
+  DRIVE_SLIDES: "Presentation",
+  DRIVE_SHEETS: "Spreadsheets",
+  DRIVE_DRAWINGS: "Drawings",
+};
 
 //Add files types
 export const addFileTypes = {
-  FILE : 'File',
-  YOU_TUBE : 'YouTube',
-  LINK : 'Link',
-  GOOGLE_DRVE: 'Google Drive',
-}
+  FILE: "File",
+  YOU_TUBE: "YouTube",
+  LINK: "Link",
+  GOOGLE_DRVE: "Google Drive",
+};
 
 //CourseWork Type Enums
 export const courseWorkType = {
-  //COURSE_WORK_TYPE_UNSPECIFIED: 
-  ASSIGNMENT : 'Assignment',
-  SHORT_ANSWER_QUESTION : 'Question',
-  MULTIPLE_CHOICE_QUESTION : 'Quiz Assignment',
-//   "Material",
-//   "Reuse post",
-}
+  //COURSE_WORK_TYPE_UNSPECIFIED:
+  ASSIGNMENT: "Assignment",
+  SHORT_ANSWER_QUESTION: "Question",
+  MULTIPLE_CHOICE_QUESTION: "Quiz Assignment",
+  //   "Material",
+  //   "Reuse post",
+};
 
 export const courseWorkAction = {
-  CREATE: 'Create',
-  EDIT: 'Edit',
-}
+  CREATE: "Create",
+  EDIT: "Edit",
+};
 
 export const actionButtonText = {
-  ADD: 'Add',
-  CREATE: 'Create',
-}
+  ADD: "Add",
+  CREATE: "Create",
+};
 
 export const roleType = {
-  TEACHER: 'teachers',
-  STUDENT: 'students',
+  TEACHER: "teachers",
+  STUDENT: "students",
   // TEACHER: {TEACHER: 'teachers'},
   // STUDENT: {STUDENT: 'students'},
-}
-
-
+};
